9ae7805286c20891c8b4272d2bc49f6ee56f3874
RFatihK <105307052+RFatihK@users.noreply.github.com>
2023-12-11 15:08:42 +0300

    İlk commit

---
+++ lib/tabs/trending_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../components/nft_card.dart';
+
+class TrendingTab extends StatelessWidget {
+  const TrendingTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NftCard(
+      imagePath: 'lib/images/apiens_3.png',
+    );
+  }
+}
